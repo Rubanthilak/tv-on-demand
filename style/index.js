163da6b5ee8e10af4007a8572adbb09bb6574d01
Ruban Thilak <rubanthilak.24@gmail.com>
2020-11-06 15:34:48 +0530

    basic template setup

---
+++ style/index.js
@@ -0,0 +1,3 @@
+import './style.scss'
+import './js/const'
+import './js/main'
---
+++ style/js/const.js
@@ -0,0 +1,9 @@
+const movieList = [{
+    "Title":"Harry Potter and the Deathly Hallows: Part 2",
+    "Year":"2011",
+    "imdbID":"tt1201607",
+    "Type":"movie",
+    "featured":true,
+    "popular":false,
+    "toprated":true
+}];
