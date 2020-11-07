b19953b03a22fb0640851c8c3d6205aed8cdacfd
Ruban Thilak <rubanthilak.24@gmail.com>
2020-11-07 12:15:01 +0530

    completed the card and hover effect

--- style/js/const.js
+++ style/js/const.js
@@ -1,9 +1,11 @@
-const movieList = [{
-    "Title":"Harry Potter and the Deathly Hallows: Part 2",
-    "Year":"2011",
-    "imdbID":"tt1201607",
-    "Type":"movie",
-    "featured":true,
-    "popular":false,
-    "toprated":true
-}];
+const movieList = [
+    {
+    "Title": "Baby Driver", 
+    "Year": "2017", 
+    "imdbID":"tt3890160",
+    "Poster":"https://m.media-amazon.com/images/M/MV5BMjM3MjQ1MzkxNl5BMl5BanBnXkFtZTgwODk1ODgyMjI@._V1_SX300.jpg",
+    "featured": true,
+    "popular": false,
+    "toprated": true
+    }
+];
